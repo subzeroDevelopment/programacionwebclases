86e840f339fb202c8a856304358cd87ed2b38d31
neomatrix <josuecevedo@gmail.com>
2015-07-19 17:36:08 -0500

    juego js
    implementando un juego usando js

--- js/juego.js
+++ js/juego.js
@@ -1,3 +1,20 @@
+var puntos = 0;
+
+
+function cargar(){
+var panel = document.getElementById("elementos")
+var grafico = document.getElementById("final")
+var boton = document.getElementById("boton")
+var letrero = document.getElementById("intro")
+var resultado = document.getElementById("resultado")
+panel.innerHTML ="<article id=piedra onclick=juego(0)> <img src=piedra.png> </article><article id=tijeras onclick=juego(1) > <img src=tijera.png> </article><article id=papel onclick=juego(2)  > <img src=papel.png> </article>"
+
+grafico.innerHTML =" ";
+boton.innerHTML =" ";
+intro.innerHTML="Elija personaje";
+resultado.innerHTML = "<p>Score: "+puntos+" <p>";
+}
+
 
 function patito (minimo, maximo)
 {
@@ -8,26 +25,74 @@ function patito (minimo, maximo)
 function juego(seleccion){
 
   var piedra = 0;
-var papel = 1;
-var  tijera = 2;
+var papel = 2;
+var  tijera = 1;
 
-var opciones = ["piedra", "papel", "tijera"];
+var panel = document.getElementById("elementos")
+var grafico = document.getElementById("final")
+var resultado = document.getElementById("resultado")
+var boton = document.getElementById("boton")
+var letrero = document.getElementById("intro")
+letrero.innerHTML = "<p></p>";
+
+var opciones = ["piedra","tijera", "papel"];
 
 var opcionmaquina = patito(0,2);
 
 var opcionUsuario;
 opcionUsuario = seleccion;
 
-  var resultado = document.getElementById("resultado")
 
-alert("Elegiste"+ opciones[opcionUsuario]);
-alert("javascritp eligio"+ opciones[opcionmaquina]);
+panel.innerHTML = "<article> <img src="+opciones[opcionUsuario]+".png>Usuario</article> VS <article> <img src="+opciones[opcionmaquina]+".png>Maquina</article>";
 
-if(opcionUsuario== piedra){
-if(opcionmaquina == piedra){
-resultado.innerText = "empate";
+if(opcionUsuario == opcionmaquina){
+
+resultado.innerHTML = "<p>[Empate]"+"  "+"Score: "+puntos+" <p>";
+
 }
+
+if(opcionUsuario < opcionmaquina){
+
+if((opcionUsuario==0)&&(opcionmaquina==2)){
+resultado.innerHTML = "<p>[Perdiste]"+"  "+"Score: "+puntos+" <p>";
+
+}else{
+	puntos++;
+	resultado.innerHTML = "<p>[GANASTE!!]"+"  "+"Score: "+puntos+" <p>";
+
+if(opcionmaquina==1){
+	grafico.innerHTML = "<section><img src=tijerarota.png></section>";
 }
+
+if(opcionmaquina==2){
+	grafico.innerHTML = "<section><img src=papelroto.png></section>";
+}
+};
+}
+
+if(opcionUsuario > opcionmaquina){
+
+if((opcionUsuario==2)&&(opcionmaquina==0)){
+puntos++;
+resultado.innerHTML = "<p>[GANASTE!!]"+"  "+"Score: "+puntos+" <p>";
+
+}else{
+	resultado.innerHTML = "<p>[Perdiste]"+"  "+"Score: "+puntos+" <p>";
+
+if(opcionUsuario==1){
+	grafico.innerHTML = "<section><img src=tijerarota.png></section>";
+}
+if(opcionUsuario==2){
+	grafico.innerHTML = "<section><img src=papelroto.png></section>";
+}
+
+
+};
+
+}
+
+
+boton.innerHTML ="<button type=submit onclick=cargar()>Jugar_otra_vez</button> "
 
 }
 
